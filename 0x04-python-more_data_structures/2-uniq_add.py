3bb1ebbd8d2a7db8ab0c27925a144862e8f4b461
walindi <walindibarack@yahoo.com>
2023-01-16 10:10:11 +0300

    Add unique integers

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    """Adds all unique integers in a list, only once for each"""
+
+    total = 0
+
+    for integer in set(my_list):
+        total += x
+
+    return total

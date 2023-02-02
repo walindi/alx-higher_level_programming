c4df2fab136ea13cce2c106f9cc7b2337c601d40
walindi <walindibarack@yahoo.com>
2023-02-02 07:29:30 +0300

    Class with pubi

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""class defining a square"""
+
+
+class Square:
+    """class with private instance attribuute - size"""
+
+    def __init__(self, size=0):
+        """Constructor for class Square
+
+        Args:
+            size: ideally an integer but it's optional.
+                Default is 0
+        """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """public instance method
+
+        Returns the current square area
+        """
+
+        return self.__size**2

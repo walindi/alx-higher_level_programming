6012aadc713308362ce344976099482b5e6a18c8
walindi <walindibarack@yahoo.com>
2023-02-03 08:54:46 +0300

    Defines a square based on 5-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -55,7 +55,9 @@ class Square:
         Args:
             value (tuple): position of square
         """
-        if (value[0] < 0) or (value[1] < 0):
+        if (len(value) != 2 or
+                (type(value[0]) is not int) or (type(value[1]) is not int) or
+                (value[0] < 0) or (value[1] < 0)):
             raise TypeError("position must be a tuple of 2 positive integers")
 
         self.position = value

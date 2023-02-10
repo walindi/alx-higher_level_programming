a2b46b25f9c0124ce9012d88e99266f1879be1ff
walindi <walindibarack@yahoo.com>
2023-02-10 11:01:12 +0300

    Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""Doc"""
+
+
+class Base:
+    """Base of all other classes in the project
+
+    Attributes:
+        __nb_objects (int): The number of instantiated bases
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Constructor of the new base
+
+        Args:
+            id (int): id of new base
+        """
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects

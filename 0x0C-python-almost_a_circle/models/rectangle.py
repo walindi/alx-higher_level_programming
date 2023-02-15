78a47bbf829530b3e712374d7071bc19e8698576
walindi <walindibarack@yahoo.com>
2023-02-15 12:40:31 +0300

    Minor fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -22,10 +22,10 @@ class Rectangle(Base):
             TypeError: if either x or y is not an int
             ValueError: if either x or y < 0
         """
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property

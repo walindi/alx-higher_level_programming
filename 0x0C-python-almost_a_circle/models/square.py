0cd71f77b7bb10a55a5365d7e378821b7f61c6e9
walindi <walindibarack@yahoo.com>
2023-02-17 08:45:12 +0300

    Square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""inherits from Rectangle"""
+from rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """square class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Contructor for new square
+
+        Args:
+            size (int): size of the new square
+            x (int): x coordinate of new square
+            y (): y coordinate
+            id (int): id of the new square
+        """
+
+        super().__init__(self, size, x, y, id)

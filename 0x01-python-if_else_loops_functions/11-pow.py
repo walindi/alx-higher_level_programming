4989c84926a6e978454beca66f4f084c12f1dfae
walindi <walindibarack@yahoo.com>
2023-02-20 20:36:02 +0300

    Exponent

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def pow(a, b):
+    """Computes a to th power of b and returns the value
+    Args:
+        a (int): base
+        b (int): exponent
+    """
+    return (a ** b)

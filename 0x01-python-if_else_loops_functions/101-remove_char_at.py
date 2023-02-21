355d99a7e550d5fce92a423d06adc1a1533f1a0b
walindi <walindibarack@yahoo.com>
2023-02-21 16:36:58 +0300

    Remove char in string

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def remove_char_at(str, n):
+    """
+    create a copy of the string,
+    removing the character at the position n
+
+    Args:
+        str: the string to be manipulated
+        n: position in the string
+    """
+
+    if n >= 0:
+        return str[:n] + str[(n + 1):]

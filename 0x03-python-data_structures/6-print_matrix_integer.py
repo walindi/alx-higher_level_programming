86303bf5b228a8a0d96dfa7b757bc9906daa0d08
walindi <walindibarack@yahoo.com>
2023-02-21 20:04:54 +0300

    Print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,9 @@
 def print_matrix_integer(matrix=[[]]):
     """Prints a matrix of integers"""
 
+    if matrix == [[]]:
+        print()
+
     for List in matrix:
         for num in List:
             if num != List[-1]:

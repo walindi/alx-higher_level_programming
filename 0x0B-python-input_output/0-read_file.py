576bcf067047266f79ca807cfb8949dfec40fa3e
walindi <walindibarack@yahoo.com>
2023-03-09 05:57:42 +0300

    Read text file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def read_file(filename=""):
+    """Reads a text file(UTF8) and prints it to stdout"""
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end="")

df939b403bf7dd227200e9b03d75e98ff1f764f3
walindi <walindibarack@yahoo.com>
2023-03-09 10:45:05 +0300

    Load, add, save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module 7-add_item.py
+Adds all arguments to a list and then saves them to a file"""
+
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
+import json
+import sys
+
+filename = "add_item.json"
+
+try:
+    items = load_from_json_file(filename)
+except FileNotFoundError:
+    items = []
+
+for arg in sys.argv[1:]:
+    items.append(arg)
+
+save_to_json_file(items, filename)
